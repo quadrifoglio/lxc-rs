3774a9b280e397eeafab88d846ca357bb90b8d82
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-09-08 15:31:29 +0200

    started container listing

--- lxc/src/lib.rs
+++ lxc/src/lib.rs
@@ -2,7 +2,17 @@
 
 extern crate lxc_sys as lib;
 
-use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::ffi::{CStr, CString};
+
+/// Custom error type for this library.
+#[derive(Debug)]
+pub enum Error {
+    Unknown
+}
+
+/// Custom result type for this library.
+pub type Result<T> = std::result::Result<T, Error>;
 
 /// Determine the version of LXC currently
 /// in use.
@@ -10,6 +20,60 @@ pub fn get_version() -> &'static str {
     unsafe {
         CStr::from_ptr(lib::lxc_get_version())
             .to_str().unwrap()
+    }
+}
+
+/// LXC Container object representation.
+#[derive(Debug)]
+pub struct Container {
+    handle: *mut lib::lxc_container,
+
+    /// Name of the container.
+    pub name: String,
+}
+
+impl Container {
+    /// Create a Rust container object based on
+    /// liblxc's lxc_container C struct.
+    fn from_raw(raw: *mut lib::lxc_container) -> Container {
+        unsafe {
+            let c = *raw;
+
+            Container {
+                handle: raw,
+                name: CString::from_raw(c.name).into_string().unwrap(),
+            }
+        }
+    }
+
+    /// Get a list of defined LXC containers in the specified
+    /// lxcpath.
+    pub fn list(lxcpath: &str) -> Result<Vec<Container>> {
+        unsafe {
+            let mut conts = 0 as *mut *mut lib::lxc_container;
+
+            let count = lib::list_defined_containers(
+                lxcpath.as_ptr() as *const c_char,
+                0 as *mut *mut *mut c_char,
+                &mut conts
+            );
+
+            if count < 0 {
+                return Err(Error::Unknown);
+            }
+            else if count == 0 {
+                return Ok(Vec::new())
+            }
+
+            let mut vec = Vec::with_capacity(count as usize);
+
+            for i in 0..count {
+                let elem = *conts.offset(i as isize);
+                vec.push(Container::from_raw(elem));
+            }
+
+            Ok(vec)
+        }
     }
 }
 
--- lxc/src/tests.rs
+++ lxc/src/tests.rs
@@ -4,3 +4,9 @@
 fn test_get_version() {
     assert!(super::get_version().len() > 0);
 }
+
+#[test]
+fn test_list_containers() {
+    let containers = super::Container::list("/var/lib/lxc");
+    assert!(containers.is_ok());
+}
