6b55b658a854a43b255e34f19a9928c5463e4442
quadrifoglio <quadrifoglio.clement@protonmail.com>
2018-01-20 20:50:36 +0100

    tests cleanup: using unwrap

--- lxc/src/tests.rs
+++ lxc/src/tests.rs
@@ -10,38 +10,34 @@ fn get_version() {
 #[test]
 fn create_get_start_freeze_unfreeze_stop_destroy_container() {
     // Create container
-    let ct = Container::create("/var/lib/lxc", "fromage", Template::new("debian"));
-    assert!(ct.is_ok());
+    let ct = Container::create("/var/lib/lxc", "fromage", Template::new("debian")).unwrap();
 
     // Verify it exists
-    let exists = Container::exists("/var/lib/lxc", "fromage");
-    assert_eq!(exists, true);
+    assert!(Container::exists(LXC_PATH, "fromage"));
 
     // Verify we can get its informations
-    assert!(Container::get("/var/lib/lxc", "fromage").is_ok());
-
-    let ct = ct.unwrap();
-    assert_eq!(ct.name, "fromage".to_owned());
+    let ct = Container::get("/var/lib/lxc", "fromage").unwrap();
+    assert_eq!(ct.name.as_str(), "fromage");
 
     // Verify that it can be started
-    assert!(ct.start().is_ok());
+    ct.start().unwrap();
 
     // Verify its state
     assert!(ct.is_running());
     assert_eq!(ct.state(), "RUNNING");
 
     // Verify that it can be frozen
-    assert!(ct.freeze().is_ok());
+    ct.freeze().unwrap();
     assert_eq!(ct.state(), "FROZEN");
 
     // Verify that it can be unfrozen
-    assert!(ct.unfreeze().is_ok());
+    ct.unfreeze().unwrap();
 
     // Verify that it can be stopped
-    assert!(ct.stop().is_ok());
+    ct.stop().unwrap();
 
     // Verify it can be destroyed
-    assert!(ct.destroy().is_ok());
+    ct.destroy().unwrap();
 }
 
 #[test]
@@ -53,60 +49,48 @@ fn create_download_container() {
         .option("-a", "amd64");
 
     // Create container
-    let ct = Container::create("/var/lib/lxc", "reblochon", template);
-    assert!(ct.is_ok());
+    let ct = Container::create("/var/lib/lxc", "reblochon", template).unwrap();
 
     // Destroy container
-    let ct = ct.unwrap();
-    assert!(ct.destroy().is_ok());
+    ct.destroy().unwrap();
 }
 
 #[test]
 fn create_config_container() {
     // Create a container
-    let ct = Container::create("/var/lib/lxc", "calice", Template::new("debian"));
-    assert!(ct.is_ok());
-    let ct = ct.unwrap();
+    let ct = Container::create("/var/lib/lxc", "calice", Template::new("debian")).unwrap();
 
     // Verify that it has a valid configration file
-    let conf_path = ct.get_config_file_name();
-
-    assert!(conf_path.is_ok());
-    assert_eq!(conf_path.unwrap().as_str(), "/var/lib/lxc/calice/config");
+    let conf_path = ct.get_config_file_name().unwrap();
+    assert_eq!(conf_path.as_str(), "/var/lib/lxc/calice/config");
 
     // Verify that we can get & set configuration values
-    let val = ct.get_config_item("lxc.utsname");
-    assert!(val.is_ok());
-    assert_eq!(val.unwrap().as_str(), "calice");
-    assert!(ct.set_config_item("lxc.utsname", "tamer").is_ok());
+    let val = ct.get_config_item("lxc.utsname").unwrap();
+    assert_eq!(val.as_str(), "calice");
+
+    ct.set_config_item("lxc.utsname", "tamer").unwrap();
 
     let val = ct.get_config_item("lxc.utsname").unwrap();
     assert_eq!(val.as_str(), "tamer");
 
     // Destroy it
-    assert!(ct.destroy().is_ok());
+    ct.destroy().unwrap();
 }
 
 #[test]
 fn create_snapshot_restore_container() {
     // Create a container
-    let ct = Container::create("/var/lib/lxc", "caribou", Template::new("debian"));
-    assert!(ct.is_ok());
-    let ct = ct.unwrap();
+    let ct = Container::create("/var/lib/lxc", "caribou", Template::new("debian")).unwrap();
 
     // Verify that it can be started and stopped
-    assert!(ct.start().is_ok());
-    assert!(ct.stop().is_ok());
+    ct.start().unwrap();
+    ct.stop().unwrap();
 
     // Try taking a snapshot
-    let snap = ct.snapshot(None);
-    assert!(snap.is_ok());
+    ct.snapshot(None).unwrap();
 
     // Verify that the listing contains the snapshot
-    let snaps = ct.snapshot_list();
-    assert!(snaps.is_ok());
-
-    let snaps = snaps.unwrap();
+    let snaps = ct.snapshot_list().unwrap();
     assert!(snaps.len() > 0);
 
     // Verify snapshot's information
@@ -115,11 +99,11 @@ fn create_snapshot_restore_container() {
     assert!(snap.created.len() > 0);
 
     // Restore the created snapshot
-    assert!(ct.snapshot_restore(snap.name.as_str(), ct.name.as_str()).is_ok());
+    ct.snapshot_restore(snap.name.as_str(), ct.name.as_str()).unwrap();
 
     // Destroy the snapshot
-    assert!(ct.snapshot_destroy(snap.name.as_str()).is_ok());
+    ct.snapshot_destroy(snap.name.as_str()).unwrap();
 
     // Destroy the container
-    assert!(ct.destroy().is_ok());
+    ct.destroy().unwrap();
 }
